4893f1a4e52fae7a0a4d79c2895512d51d55e33d
Joseph Black <josephconradblack@gmail.com>
2018-08-20 14:10:49 +1000

    Update prop-types comments for Container

--- src/components/Layout/Container.js
+++ src/components/Layout/Container.js
@@ -18,12 +18,14 @@ const Container = ({ children, fluid, widescreen, fullHd, ...rest }) => {
 };
 
 Container.propTypes = {
-  /** test */
+  /** Children content to render inside the container. */
   children: PropTypes.node,
+  /** Maintain 32px margins on the side of the container. */
   fluid: PropTypes.bool,
+  /** Full-width container until the $widescreen breakpoint is hit. */
   widescreen: PropTypes.bool,
+  /** Full-width container until the $fullHd breakpoint is hit. */
   fullHd: PropTypes.bool,
-  /** test2 */
   ...withTypographyPropTypes
 };
 
