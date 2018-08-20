cb827e161154b7c994dcfaedc91baf070db65303
Joseph Black <josephconradblack@gmail.com>
2018-08-20 13:56:07 +1000

    Add bulma scss to autogenerated documentation

--- src/util/DoczWrapper.js
+++ src/util/DoczWrapper.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import '../styles.scss';
-// import './DoczWrapper.scss';
+import './styles.scss';
+import './DoczWrapper.scss';
 
 const DoczWrapper = ({ children }) => (
   <React.Fragment>{children}</React.Fragment>
